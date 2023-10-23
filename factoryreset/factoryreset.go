509fa04ad0126ca525540805cf56741633e67088
prinikasn <117314826+prinikasn@users.noreply.github.com>
2023-10-23 13:35:17 -0700

    Add factory reset (#15)
    * add factory reset
    
    * remove RetainCerts for now will add in later PR

---
+++ factoryreset/factoryreset.go
@@ -0,0 +1,37 @@
+package factoryreset
+
+import (
+	"context"
+
+	frpb "github.com/openconfig/gnoi/factory_reset"
+
+	"github.com/openconfig/gnoigo/internal"
+)
+
+// StartOperation represents the parameters of a FactoryReset Start operation.
+type StartOperation struct {
+	req *frpb.StartRequest
+}
+
+// NewStartOperation creates an empty StartOperation.
+func NewStartOperation() *StartOperation {
+	return &StartOperation{req: &frpb.StartRequest{}}
+}
+
+// FactoryOS instructs the target to rollback the OS to the
+// same version as it shipped from factory.
+func (s *StartOperation) FactoryOS(fos bool) *StartOperation {
+	s.req.FactoryOs = fos
+	return s
+}
+
+// ZeroFill instructs the target to zero fill persistent storage state data.
+func (s *StartOperation) ZeroFill(zf bool) *StartOperation {
+	s.req.ZeroFill = zf
+	return s
+}
+
+// Execute performs the Start operation.
+func (s *StartOperation) Execute(ctx context.Context, c *internal.Clients) (*frpb.StartResponse, error) {
+	return c.FactoryReset().Start(ctx, s.req)
+}
